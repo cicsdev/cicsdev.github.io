66a32cf829da40f0a8a8a463f9110eb05c076664
James Taylor <jamest@uk.ibm.com>
2023-07-04 14:41:18 +0000

    Add docs link to 404 page
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import { FourOhFour } from 'gatsby-theme-carbon';
 
 const links = [
   { href: '/', text: 'CICS on GitHub Homepage' },
+  { href: 'https://www.ibm.com/docs/en/cics-ts', text: 'CICS Transaction Server for z/OS documentation' },
 ];
 
 const Custom404 = () => <FourOhFour links={links} />;
