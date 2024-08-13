5b997f2a4b503c4c6cedba2012139afae34ce87a
James Taylor <jamest@uk.ibm.com>
2024-08-13 16:33:34 +0100

    Remove Gatsby website
    The https://cicsdev.github.io site did not add enough functionality
    compared to the GitHub repositories view to justify the maintenance
    
    The cicsdev organisation now has a readme to provide an alternative
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- src/components/Repositories/index.js
+++
@@ -1,3 +0,0 @@
-import Repositories from './Repositories';
-
-export default Repositories;
--- src/gatsby-theme-carbon/components/NextPrevious/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CustomNextPrevious = () => (
-  <></>
-);
-
-export default CustomNextPrevious;
